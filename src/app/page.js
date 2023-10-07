5c9502f37e1af386151f8365f46d9fce07149bd5
Hanny <35860420+hridayarajbanshi@users.noreply.github.com>
2023-10-07 13:29:32 +0545

    i

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,24 @@
-import Image from 'next/image'
-import { ChakraProvider } from '@chakra-ui/react'
+import React from 'react'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-
+    <>
+    <div className='mainSection grid grid-cols-2 '>
+      <div className='mainContent'>
+        <h1 className='text-3xl text-center'><b >Explore</b>  the <b>SciMarket</b> for Open Science Project</h1>
       </div>
-    </main>
+      <div className='mainImg'>
+       <div className='ImgBox1 '>
+        <img src='https://images.pexels.com/photos/2156/sky-earth-space-working.jpg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' width={"350px"} height={"auto"} className='rounded-2xl'/>
+       </div>
+       <div className='ImgBox2'>
+        
+       </div>
+       <div className='ImgBox3'>
+        
+       </div>
+      </div>
+    </div>
+    </>
   )
 }
