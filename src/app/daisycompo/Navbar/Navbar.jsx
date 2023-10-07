275c639b410ecb08271079a33ee1916d90f831e1
Sugam Pokhrel <114973396+sugam-pokhrel@users.noreply.github.com>
2023-10-07 23:07:05 +0545

    changed

--- src/app/daisycompo/Navbar/Navbar.jsx
+++ src/app/daisycompo/Navbar/Navbar.jsx
@@ -34,12 +34,10 @@ const Navbar = () => {
               </div>
             </label>
             <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-              <li>
-                <a className="justify-between">
-                  Profile
-                  <span className="badge">New</span>
-                </a>
-              </li>
+              
+              <li><button>Profile</button></li>
+
+            
               <li><button>Settings</button></li>
               
               <li><button onClick={() => signOut()}>Logout</button></li>
