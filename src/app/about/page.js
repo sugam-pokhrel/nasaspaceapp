d853e96bf25628717b4550d839b5446b49ed32b0
Sugam Pokhrel <114973396+sugam-pokhrel@users.noreply.github.com>
2023-10-07 22:24:06 +0545

    all

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,44 +1,62 @@
+"use client"
+import Navbar from '../daisycompo/Navbar/Navbar';
+import React from 'react';
 
-import Navbar from '../daisycompo/Navbar/Navbar'
-import React from 'react'
-const page = () => {
+const Page = () => {
   return (
-  <>
-  <Navbar/>
-  <h1 className='text-center'>About</h1>  
-  <div className='aboutSection grid grid-cols-2'>
-   
-    <div className='aboutSectionLeft'>
-      <h2 className='text-3xl '>What is <b >SciMarket</b> ?</h2>
-      <p>SciMarket is a web app that addresses the challenge of fostering openness, transparency, and interdisciplinary teamwork within the scientific community, indirectly contributing to broader space research goals It eases the process of starting a science based project as it unites researchers, promoting open science through project listings that emphasize transparency, peer review, and interdisciplinary teamwork. This web app offers project tracking, private meetings, version control, and collaboration tools while safeguarding user data. It will have additional features like paid projects and a marketplace for buying and selling materials.</p>
-      </div>
-      <div className='aboutSectionRight'>
-        <img src='./sciMart.png' width='auto'></img>
+    <>
+      <Navbar />
+      <h1 className='text-center'>About</h1>
+      <div className='aboutSection grid grid-cols-2'>
+        <div className='aboutSectionLeft'>
+          <h2 className='text-3xl'>
+            What is <b>SciMarket</b> ?
+          </h2>
+          <p>
+            SciMarket is a web app that addresses the challenge of fostering
+            openness, transparency, and interdisciplinary teamwork within the
+            scientific community, indirectly contributing to broader space
+            research goals It eases the process of starting a science based
+            project as it unites researchers, promoting open science through
+            project listings that emphasize transparency, peer review, and
+            interdisciplinary teamwork. This web app offers project tracking,
+            private meetings, version control, and collaboration tools while
+            safeguarding user data. It will have additional features like paid
+            projects and a marketplace for buying and selling materials.
+          </p>
+          <img src='./sciMart.png' width='auto' />
         </div>
-  </div>
-  <div className='aboutSection2 grid grid-cols-2'>
-    <div className='aboutSectionLeft2'>
-    <img src='https://i.pinimg.com/736x/6c/a2/77/6ca277362ea9818d785e045de853402c.jpg' width='auto'></img>
-
-      </div>
-      <div className='aboutSectionRigh2'>
-        <h2 className='text-3xl'>Why <b>SciMarket</b>? </h2>
-        <p><b>Facilitating Project Initiation:</b> SciMarket streamlines the start of science-based projects by connecting researchers, making it easier for them to embark on scientific endeavors.
-        <br />
-<b>Promoting Open Science:</b> The platform encourages open science by featuring project listings that emphasize transparency, peer review, and interdisciplinary teamwork. This fosters a culture of sharing and collaboration in the scientific community.
-<br />
-<b>
-Comprehensive Project Management: </b>SciMarket offers a suite of project management tools, including project tracking, private meetings, and version control, to help researchers efficiently manage their scientific projects.
-<br />
-
-<b>Data Security:</b> While promoting collaboration, SciMarket also prioritizes user data security to safeguard sensitive research information.
-<br />
-<b>Future Enhancements: </b>The platform is poised to introduce additional features such as paid projects and a marketplace for buying and selling materials, enhancing its utility for scientists. </p>
+        <div className='aboutSectionRight'>
+          <h2 className='text-3xl'>
+            Why <b>SciMarket</b>?
+          </h2>
+          <p>
+            <b>Facilitating Project Initiation:</b> SciMarket streamlines the
+            start of science-based projects by connecting researchers, making it
+            easier for them to embark on scientific endeavors.
+            <br />
+            <b>Promoting Open Science:</b> The platform encourages open science
+            by featuring project listings that emphasize transparency, peer
+            review, and interdisciplinary teamwork. This fosters a culture of
+            sharing and collaboration in the scientific community.
+            <br />
+            <b>Comprehensive Project Management: </b>SciMarket offers a suite
+            of project management tools, including project tracking, private
+            meetings, and version control, to help researchers efficiently manage
+            their scientific projects.
+            <br />
+            <b>Data Security:</b> While promoting collaboration, SciMarket also
+            prioritizes user data security to safeguard sensitive research
+            information.
+            <br />
+            <b>Future Enhancements: </b>The platform is poised to introduce
+            additional features such as paid projects and a marketplace for
+            buying and selling materials, enhancing its utility for scientists.
+          </p>
         </div>
-  </div>
-  </>
-  
-    )
-}
+      </div>
+    </>
+  );
+};
 
-export default page
+export default Page;
--- src/app/daisycompo/Navbar/Navbar.jsx
+++ src/app/daisycompo/Navbar/Navbar.jsx
@@ -1,57 +1,56 @@
 import Link from 'next/link';
 import React from 'react';
-import { useSession, signIn, signOut } from "next-auth/react";
-
+import { signOut } from 'next-auth/react';
 const Navbar = () => {
-  const { data: session } = useSession();
+  // Replace with your session logic
+ // Replace with your sign out logic
 
   return (
     <>
       <div className="navbar bg-base-100">
         <div className="flex-1">
-          <Link href={"/"}><a><img src="./sciMart.png" width={"100px"}></img></a></Link>
-          <ul tabIndex={0} className="menu menu-md dropdown-content mt-3 z-[1] p-5 flex-space-between w-100 flex-row">
-            <li><Link href={"./about"}><a className="mr-4">About</a></Link></li>
-            <li><Link href={"./features"}><a className="mr-4">Features</a></Link></li>
+          <Link href="/">
+            <img src="./sciMart.png" width="100px" />
+          </Link>
+          <ul tabIndex={0} className="menu menu-md dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 flex-row">
+            <li>
+              <Link href="/about">
+               About
+              </Link>
+            </li>
+            <li>
+              <Link href="/features">
+                Features
+              </Link>
+            </li>
           </ul>
         </div>
+
         <div className="flex-none">
-          <div className="dropdown dropdown-end"></div>
           <div className="dropdown dropdown-end">
-            <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+            <label tabIndex={0} className="btn btn-ghost btn-circle">
               <div className="w-10 rounded-full">
                 <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
               </div>
-              <div className="flex-none">
-                <div className="dropdown dropdown-end"></div>
-                <div className="dropdown dropdown-end">
-                  <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-                    <div className="w-10 rounded-full">
-                      <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
-                    </div>
-                  </label>
-                  <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                    <li>
-                      <a className="justify-between">
-                        Profile
-                        <span className="badge">New</span>
-                      </a>
-                    </li>
-                    <li><button>Settings</button></li>
-                    {session ? (
-                      <li><button onClick={() => signOut()}>Logout</button></li>
-                    ) : (
-                      <li><button onClick={() => signIn()}>Login</button></li>
-                    )}
-                  </ul>
-                </div>
-              </div>
             </label>
+            <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+              <li>
+                <a className="justify-between">
+                  Profile
+                  <span className="badge">New</span>
+                </a>
+              </li>
+              <li><button>Settings</button></li>
+              
+              <li><button onClick={() => signOut()}>Logout</button></li>
+              
+              
+            </ul>
           </div>
         </div>
       </div>
     </>
-  )
-}
+  );
+};
 
 export default Navbar;
