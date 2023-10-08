d6d49b6749b966e6dd31933a1f4a1153c0ca1dcd
Sugam Pokhrel <114973396+sugam-pokhrel@users.noreply.github.com>
2023-10-08 08:36:36 +0545

    add

---
+++ src/app/api/getpost/route.js
@@ -0,0 +1,94 @@
+import {  getServerSession} from "next-auth";
+import prisma from '../../../../utils/prisma'
+import { NextResponse } from 'next/server';
+
+export async function GET(request,res){
+
+
+    let data=await prisma.projects.findMany({})
+    return NextResponse.json({data:data},{status:200})
+}
+export async function POST(request,res) {
+    var session= await getServerSession();
+    let response = NextResponse.next()
+    console.log(session)
+
+     
+
+    if (session !== null) {
+        const email = session.user.email;
+        const data=await request.json();
+
+        console.log(data)
+
+        let ans=await prisma.projects.findFirst({
+           where:{
+            id:data.id
+              }
+              
+        })
+       return NextResponse.json({data:ans},{status:200})
+
+
+   
+
+
+
+
+
+
+// if (data.name && data.phone && data.ElecricityId && data.ElectricityScNo && data.ElectricityOfficeName && data.transactionID !== '') {
+//     // All variables are not empty, and transactionID is not empty.
+//     // You can proceed with your code here.
+//     console.log('all good')
+    
+// } else {
+//   console.log('something went wrong')
+//   return  NextResponse.json({message:'err'},{status:404})
+   
+
+
+
+    
+   
+  
+
+// }
+
+
+//     if (Object.keys(data.address).every(function(x) { return data.address[x]===''||data.address[x]===null;}) === false) {
+//    console.log('has something');
+// } else {
+//    console.log('nothing');
+// }
+
+    
+
+
+
+  }
+  
+
+
+
+    
+
+
+   
+    // try{
+
+    //     // const body=await request.json();
+    //     // const {username,password,email}=body;
+
+    //    const user= await prisma.user.findUnique({
+           
+    //    });
+
+    //     return Response.json({message:"success"})
+
+    // }catch(e){
+    //     return NextResponse.error();
+    // }
+
+}
+
